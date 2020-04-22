271fbbdac6492f23e2ad56ebe7af40878c34a4ab
iamfortune <ikechifortune@gmail.com>
2020-04-22 10:57:37 +0100

    modified files

--- src/components/header.js
+++ src/components/header.js
@@ -7,8 +7,8 @@ const Header = ({name, contactEmail})=>{
                 <Link to="/"><h1>{name}.</h1></Link>
                 <nav>
                     <ul>
-                        <li><Link to="/about">about</Link></li>
-                        <li><a href={`mailto:${contactEmail}`}>contact</a></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><a href={`mailto:${contactEmail}`}>Contact</a></li>
                     </ul>
                 </nav>
             </div>
